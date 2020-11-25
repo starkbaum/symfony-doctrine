d1df0db5cfc9651657370362c265230933719d21
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-25 12:14:07 +0100

    Chapter 15: automatic controller queries: param converter

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -85,25 +85,23 @@ EOF
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
-     * @param $slug
-     * @param MarkdownHelper $markdownHelper
-     * @param EntityManagerInterface $entityManager
+     * @param Question $question
      * @return Response
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
 
-        $repository = $entityManager->getRepository(Question::class);
+        //$repository = $entityManager->getRepository(Question::class);
 
-        /** @var Question|null $question */
-        $question = $repository->findOneBy(['slug' => $slug]);
+        ///** @var Question|null $question */
+        //$question = $repository->findOneBy(['slug' => $slug]);
 
-        if (!$question) {
-            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
-        }
+        //if (!$question) {
+        //    throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
+        //}
 
         //dd($question);
 
