bc33175ff859b5f9bf4931342574b5cc6eb37c96
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-25 13:44:48 +0100

    Chapter 21: foundry tricks

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Question;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,5 +11,9 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         QuestionFactory::new()->createMany(20);
+
+        QuestionFactory::new()
+            ->unpublished()
+            ->createMany(5);
     }
 }
--- src/Factory/QuestionFactory.php
+++ src/Factory/QuestionFactory.php
@@ -4,6 +4,7 @@ namespace App\Factory;
 
 use App\Entity\Question;
 use App\Repository\QuestionRepository;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 use Zenstruck\Foundry\RepositoryProxy;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
@@ -26,22 +27,20 @@ final class QuestionFactory extends ModelFactory
         // TODO inject services if required (https://github.com/zenstruck/foundry#factories-as-services)
     }
 
+    public function unpublished(): self
+    {
+        return $this->addState(['askedAt' => null]);
+    }
+
     protected function getDefaults(): array
     {
         return [
-            'name' => 'Missing pants',
-            'slug' => 'missing-pants-' . rand(0, 1000),
-            'question' => <<<EOF
-Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
-to make my dishes wash themselves. But while I was casting it,
-I slipped a little and I think `I also hit my pants with the spell`.
-When I woke up this morning, I caught a quick glimpse of my pants
-opening the front door and walking out! I've been out all afternoon
-(with no pants mind you) searching for them.
-Does anyone have a spell to call your pants back?
-
-EOF,
-            'askedAt' => rand(1, 19) > 2 ? new \DateTime(sprintf('-%d days', rand(1,100))) : null,
+            'name' => self::faker()->realText(50),
+            'question' => self::faker()->paragraphs(
+                self::faker()->numberBetween(1, 4),
+                true
+            ),
+            'askedAt' => self::faker()->dateTimeBetween('-100 days', '-1 minute'),
             'votes' => rand(-20, 50),
         ];
     }
@@ -50,7 +49,12 @@ EOF,
     {
         // see https://github.com/zenstruck/foundry#initialization
         return $this
-            // ->afterInstantiate(function(Question $question) {})
+            ->afterInstantiate(function(Question $question) {
+                if (!$question->getSlug()) {
+                    $slugger = new AsciiSlugger();
+                    $question->setSlug($slugger->slug($question->getName()));
+                }
+            })
         ;
     }
 
