d22f0c5e33bfde15b7cd77e7b01fde06befe0104
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-25 10:49:27 +0100

    Chapter 7: migrations

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -23,7 +23,7 @@ class Question
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $slug;
 
